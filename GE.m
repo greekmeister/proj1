2a7c2ea4be27edca89160e15b4073e002394f6b7
unknown <fkakouris@gmail.com>
2019-06-03 21:12:25 +0300

    progress

--- GE.m
+++ GE.m
@@ -1,12 +1,8 @@
 function [L, U] = GE(A)
-n = size(A,1);
-L = eye(n,n);
-U = A;
-for i=1:n-1
-    for j=i+1:n
-        L(j,i) = U(j, i)/U(i,i);
-        for k=i:n
-            U(j,k) = U(j,k) - L(j,i)*U(i,k);
-        end
-    end
+n=size(A,1);
+for k=1:n-1
+  A(k+1:n,k) = A(k+1:n,k)/A(k,k);
+  A(k+1:n,k+1:n) = A(k+1:n,k+1:n) - A(k+1:n,k)*A(k,k+1:n);
 end
+L = eye(n)+tril(A,-1);
+U = triu(A);
--- ask1.m
+++ ask1.m
@@ -1,5 +1,9 @@
-print(4+2)
-a=24;
-b=415;
-A=[112, 21]
-B=12;
+A = eye(7,7) + diag(2*ones(1,6),1) + diag(2*ones(1,6),-1) + diag(3*ones(1,5),2) + diag(3*ones(1,5),-2);
+[L,U] = GEm(A);
+
+d=1;
+x = diag(U);
+for i=1:7
+    d=d*x(i);
+end
+fprintf("Det: %f", d)
